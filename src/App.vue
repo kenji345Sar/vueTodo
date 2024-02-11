ee520165a2675f5221db99f75b488298170fce6f
Apple <apple@appurunoMacBook-Pro.local>
2024-02-11 11:53:01 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div id="app">
+    <ListRow v-for="task in tasks" :key="task.id" :task="task.title" :isCheck="task.checked" @toggle="toggleTask" />
+  </div>
+</template>
+
+<script>
+import ListRow from './components/ListRow.vue';
+
+export default {
+  components: {
+    ListRow
+  },
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: '散歩', checked: true },
+        { id: 2, title: '料理', checked: false },
+        { id: 3, title: '筋トレ', checked: true },
+      ],
+    };
+  },
+  methods: {
+    toggleTask(taskTitle) {
+      const task = this.tasks.find(t => t.title === taskTitle);
+      if (task) {
+        task.checked = !task.checked;
+      }
+    },
+  },
+};
+</script>
+
---
+++ src/components/ListRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div @click="handleClick">
+    <span v-if="isCheck">☑︎</span>
+    <span v-else>□</span>
+    <span :style="{ textDecoration: isCheck ? 'line-through' : 'none' }">{{ task }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: String,
+    isCheck: Boolean
+  },
+  methods: {
+    handleClick() {
+      this.$emit('toggle', this.task);
+    }
+  }
+};
+</script>
